1669d3d4a0eeff716652418c6c852e00c53ab05d
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2026-01-05 07:49:47 +0530

    recipes and tasks

--- manifest/manifest.js
+++ manifest/manifest.js
@@ -2,28 +2,36 @@ const result = [];
 
 async function executeTask(taskName) {
   console.log(taskName, " started");
-  const content = await Deno.readTextFile(`./${taskName}.txt`);
+  const content = await Deno.readTextFile(`./manifest/${taskName}.txt`);
   const sum = content.split("\n")
     .reduce((n1, n2) => parseInt(n1) + parseInt(n2));
   console.log(taskName, " finished");
   return sum;
 }
 
-const executeLine = (line) => {
+const executeLine = (line, fn) => {
   const task = line.split(",").map((l) => l.trim());
-  return Promise.all(task.map(executeTask));
+  return Promise.all(task.map(fn));
 };
 
-async function executeRecipe(reciepeFile) {
-  console.log("reciepe started");
-  const content = await Deno.readTextFile(reciepeFile);
-  const lines = content.split('\n');
+async function executeEachReciepe(reciepeName) {
+  const content = await Deno.readTextFile(`./manifest/${reciepeName}.txt`);
+  const lines = content.split("\n").map((l) => l.trim());
   for (const line of lines) {
-    console.log("please wait...");
-    const t = await executeLine(line);
-    result.push(t);
+    const value = await executeLine(line, executeTask);
+    result.push(value);
   }
-  console.log("Receipe Completed");
 }
 
-await executeRecipe('reciepes.txt');
+async function executeRecipe(file) {
+  console.log("Task started");
+  const content = await Deno.readTextFile(file);
+  const lines = content.split("\n");
+  for (const line of lines) {
+    console.log("please wait...");
+    await executeLine(line, executeEachReciepe);
+  }
+  console.log("Reciepe Completed");
+}
+
+await executeRecipe("./manifest/tasks.txt");
